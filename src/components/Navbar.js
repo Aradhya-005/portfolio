684c4e4db0eda0177b8dea37f0442cc41294a1fc
aradhya <aradhyay868@gmail.com>
2025-09-13 10:12:18 +0530

    changes

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,15 +1,18 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
 import star from "../assets/star.svg";
+import MenuIcon from '../assets/Menu.svg'
+import { useState } from "react";
 import { useLocation } from "react-router-dom";
-// import { RxHamburgerMenu } from "react-icons/rx";
 
 
 function Navbar() {
   const location = useLocation();
   const isProjectsPage = location.pathname === "/projects";
+  const [open, setOpen] = useState(false);
   return (
-    <nav className="navbar">
+    <>
+      <nav className="navbar">
       <div
         className="logo"
         style={{ visibility: isProjectsPage ? "hidden" : "visible" }}
@@ -35,8 +38,43 @@ function Navbar() {
           </NavLink>
         </li>
       </ul>
-      {/* <RxHamburgerMenu className="hamburger" /> */}
     </nav>
+    <nav className="mobile-navbar">
+         <div
+        className="logo"
+        style={{ visibility: isProjectsPage ? "hidden" : "visible" }}
+      >
+        <img src={star} alt="star" />
+        <p>Aradhya Yadav</p>
+      </div>
+      <div className="menu-icon " onClick={() => setOpen(!open)}>
+        <img src={MenuIcon} alt="menuicon" />
+
+      </div>
+      <ul onClick={() => setOpen(!open)} className={open ? "mobile-nav-links" : "mobile-nav-links hidden"}>
+        <li>
+          <NavLink
+            to="/"
+            className={({ isActive }) => (isActive ? "active-link" : "")}
+          >
+            About
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            to="/projects"
+            className={({ isActive }) => (isActive ? "active-link" : "")}
+          >
+            Projects
+          </NavLink>
+        </li>
+      </ul>
+
+
+    </nav>
+    
+    </>
+  
   );
 }
 
--- src/components/Project.js
+++ src/components/Project.js
@@ -3,53 +3,48 @@ import chloro from "../assets/chloro.png";
 import Pokemon from "../assets/pokemon.png";
 import Emocare from "../assets/Emocare.png";
 import CreativFi from "../assets/creative.png";
-import Vocal from "../assets/Vocal.png";
 import Navbar from "./Navbar";
+import Plane from '../assets/Plane.svg';
 
 function Project() {
   const projects = [
     {
+      className: "creativfi",
       title: "CreativFi",
       description:
         "a web app designed to connect content creators with freelancing opportunities tailored to their unique skills.",
-      techStack : "created using reactjs",
-      image: CreativFi, 
+      techStack: "Reactjs",
+      image: CreativFi,
       github: "https://github.com/Aradhya-005/CreativFi",
       preview: "https://creativfi.vercel.app/",
     },
+   
     {
-      title: "VocalRemover",
-      description:
-        "A UI of a platform which extract the vocals from a file",
-        techStack : "created using Nextjs, Mantine UI",
-      image: Vocal, 
-      github: "https://github.com/Aradhya-005/VocalRemover",
-      preview: "https://vocal-remover-gilt.vercel.app/",
-    },
-    {
+      className: "emocare",
       title: "Emocare",
       description:
         "A web app that suggests techniques to reduce stress and improve well-being.",
-        techStack : "created using Reactjs",
-      image: Emocare, 
+      techStack: "Reactjs",
+      image: Emocare,
       github: "https://github.com/Aradhya-005/Emocare",
       preview: "https://emocare-alpha.vercel.app/",
     },
     {
-      title: "Poke",
+      className: "poke",
+      title: "PokeMon",
       description:
         "A  web app that includes all the pokemon characters with search functionality ( you can search your favorite character). ",
-        techStack : "created using Nextjs",
-      image: Pokemon, 
+      techStack: "Nextjs",
+      image: Pokemon,
       github: "https://github.com/Aradhya-005/poke_mon",
       preview: "https://poke-mon-olive.vercel.app/",
     },
     {
-      title: "Cholro",
-      description:
-        "A platform related to skincare and beauty ",
-      techStack : "created using Nextjs, Tailwind CSS",
-      image: chloro, 
+      className: "chloro",
+      title: "Chloro",
+      description: "A platform related to skincare and beauty ",
+      techStack: "Nextjs, Tailwind CSS",
+      image: chloro,
       github: "https://github.com/Aradhya-005/chloro",
       preview: "https://chloro-navy.vercel.app/",
     },
@@ -57,37 +52,53 @@ function Project() {
 
   return (
     <>
-      <div className="project-array">
-      <Navbar/>
-       <div className= 'project-wrapper'>
-         {projects.map((project, index) => (
-          <div className="project-container" key={index}>
-            <div className="project-img">
-              <img src={project.image} alt={`${project.title} thumbnail`} />
-            </div>
-            <div className="project-desc">
-              <p>{project.description}</p>
-            </div>
-            <div>  <p>{project.techStack}</p></div>
-            <div className="navigations">
-              <a
-                href={project.preview}
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                Live Preview
-              </a>
-              <a
-                href={project.github}
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                GitHub
-              </a>
+      <div className="project-array ">
+        <Navbar />
+        <div className=" project-cards">
+        <div className="project-container">
+            <div>
+            <div className=" project-heading-container">
+            <h1 className="project-heading">Projects <img src={Plane} alt="plane"/></h1>
+              
             </div>
           </div>
-        ))}
-       </div>
+          {projects.map((project, index) => (
+            <div
+              className={` ${project.className} card project-card`}
+              key={index}
+            >
+              <div className="project-img">
+                <img src={project.image} alt={`${project.title} thumbnail`} />
+              </div>
+              <h2 className="project-title">{project.title}</h2>
+              <div className="project-desc">
+                <p>{project.description}</p>
+                <p className="project-tech">
+                  {" "}
+                  created using<span> {project.techStack}</span>
+                </p>
+              </div>
+              <div className="navigations">
+                <a
+                  href={project.preview}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  Live Preview
+                </a>
+                <a
+                  href={project.github}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  GitHub
+                </a>
+              </div>
+            </div>
+          ))}
+
+        </div>
+        </div>
       </div>
     </>
   );
