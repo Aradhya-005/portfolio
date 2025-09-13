d13a70e86518c16594ebb09a169b893845d7431e
aradhya <aradhyay868@gmail.com>
2025-09-13 15:56:00 +0530

    changes

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,80 +1,87 @@
-import React from "react";
-import { NavLink } from "react-router-dom";
+import React, { useState } from "react";
+import { NavLink, useLocation } from "react-router-dom";
 import star from "../assets/star.svg";
-import MenuIcon from '../assets/Menu.svg'
-import { useState } from "react";
-import { useLocation } from "react-router-dom";
-
+import MenuIcon from "../assets/Menu.svg";
+import CloseIcon from "../assets/Close.svg"; 
 
 function Navbar() {
   const location = useLocation();
   const isProjectsPage = location.pathname === "/projects";
   const [open, setOpen] = useState(false);
+
   return (
     <>
       <nav className="navbar">
-      <div
-        className="logo"
-        style={{ visibility: isProjectsPage ? "hidden" : "visible" }}
-      >
-        <img src={star} alt="star" />
-        <p>Aradhya Yadav</p>
-      </div>
-      <ul className="nav-links">
-        <li>
-          <NavLink
-            to="/"
-            className={({ isActive }) => (isActive ? "active-link" : "")}
-          >
-            About
-          </NavLink>
-        </li>
-        <li>
-          <NavLink
-            to="/projects"
-            className={({ isActive }) => (isActive ? "active-link" : "")}
-          >
-            Projects
-          </NavLink>
-        </li>
-      </ul>
-    </nav>
-    <nav className="mobile-navbar">
-         <div
-        className="logo"
-        style={{ visibility: isProjectsPage ? "hidden" : "visible" }}
-      >
-        <img src={star} alt="star" />
-        <p>Aradhya Yadav</p>
-      </div>
-      <div className="menu-icon " onClick={() => setOpen(!open)}>
-        <img src={MenuIcon} alt="menuicon" />
+        <div
+          className="logo"
+          style={{ visibility: isProjectsPage ? "hidden" : "visible" }}
+        >
+          <img src={star} alt="star" />
+          <p>Aradhya Yadav</p>
+        </div>
+        <ul className="nav-links">
+          <li>
+            <NavLink
+              to="/"
+              className={({ isActive }) => (isActive ? "active-link" : "")}
+            >
+              About
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to="/projects"
+              className={({ isActive }) => (isActive ? "active-link" : "")}
+            >
+              Projects
+            </NavLink>
+          </li>
+        </ul>
+      </nav>
 
-      </div>
-      <ul onClick={() => setOpen(!open)} className={open ? "mobile-nav-links" : "mobile-nav-links hidden"}>
-        <li>
-          <NavLink
-            to="/"
-            className={({ isActive }) => (isActive ? "active-link" : "")}
-          >
-            About
-          </NavLink>
-        </li>
-        <li>
-          <NavLink
-            to="/projects"
-            className={({ isActive }) => (isActive ? "active-link" : "")}
-          >
-            Projects
-          </NavLink>
-        </li>
-      </ul>
+      {/* Mobile Navbar */}
+      <nav className="mobile-navbar">
+        <div
+          className="logo"
+          style={{ visibility: isProjectsPage ? "hidden" : "visible" }}
+        >
+          <img src={star} alt="star" />
+          <p>Aradhya Yadav</p>
+        </div>
 
+        {/* Toggle Button */}
+        <div className="menu-icon" onClick={() => setOpen(!open)}>
+          {open ? (
+            <img src={CloseIcon} alt="close" />
+          ) : (
+            <img src={MenuIcon} alt="menu" />
+          )}
+        </div>
 
-    </nav>
-    
+        {/* Nav Links */}
+        <ul
+          className={open ? "mobile-nav-links" : "mobile-nav-links hidden"}
+          onClick={() => setOpen(false)} // closes when clicking a link
+        >
+          <li>
+            <NavLink
+              to="/"
+              className={({ isActive }) => (isActive ? "active-link" : "")}
+            >
+              About
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to="/projects"
+              className={({ isActive }) => (isActive ? "active-link" : "")}
+            >
+              Projects
+            </NavLink>
+          </li>
+        </ul>
+      </nav>
     </>
-  
   );
 }
 
